b19d521a68405b70f99a2a767019e593b84b8cae
bartek3pl <bartekh1998@op.pl>
2020-01-09 23:29:42 +0100

    Change react spinner in articles figures

--- src/components/articlesPage/ArticlesPageFigure.tsx
+++ src/components/articlesPage/ArticlesPageFigure.tsx
@@ -2,7 +2,8 @@ import React, { useState, useEffect, FunctionComponent } from 'react';
 import { Col } from 'react-bootstrap';
 import { Link as ReachLink } from '@reach/router';
 import { convertToUrl } from '../../utils/jsUtils';
-import CircleLoader from 'react-spinners/ClipLoader';
+import { css } from '@emotion/core';
+import CircleLoader from 'react-spinners/CircleLoader';
 import './ArticlesPageFigure.scss';
 
 interface IProps {
@@ -21,11 +22,23 @@ const ArticlesPageFigure: FunctionComponent<IProps> = ({
   const [loaded, setLoaded] = useState(false);
   useEffect(() => {}, [loaded]);
 
+  const spinner = css`
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    transform: translate(-50%, -50%);
+  `;
+
   return (
-    <Col xl={4} lg={6}>
-      {!loaded ? (
-        <CircleLoader size={50} color={'#fff'} loading={!loaded} />
-      ) : null}
+    <Col xl={4} lg={6} className="articles-page-column">
+      <CircleLoader
+        size={50}
+        color={'#22d2c8'}
+        css={spinner}
+        loading={!loaded}
+      />
+
       <figure
         className="figures-wrapper"
         aria-label={caption}
