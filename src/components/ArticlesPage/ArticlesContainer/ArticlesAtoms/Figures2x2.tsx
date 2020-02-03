009c131a52f3ed10e7cb4f412e776c810067fd08
bartek3pl <bartekh1998@op.pl>
2020-02-03 20:35:52 +0100

    Change Figures2x2 component

--- src/components/ArticlesPage/ArticlesContainer/ArticlesAtoms/Figures2x2.tsx
+++ src/components/ArticlesPage/ArticlesContainer/ArticlesAtoms/Figures2x2.tsx
@@ -3,14 +3,7 @@ import Figure from './Figure';
 
 const Figures2x2 = ({ children, figcaption }) => {
   const AllFourFigures = children.map((child) => (
-    <div className="figure-img">
-      <div className="article-img-overlay">
-        {child}
-        <span className="article-img-link d-block rounded">
-          <p className="title">{child.alt}</p>
-        </span>
-      </div>
-    </div>
+    <div className="figure-img">{child}</div>
   ));
 
   return <Figure figcaption={figcaption}>{AllFourFigures}</Figure>;
